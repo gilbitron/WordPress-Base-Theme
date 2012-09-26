1d23e6b7a908e03734f1a1d059cb7f35bbff6d9a
Gilbert Pellegrom <drummermanny@gmail.com>
2012-09-26 14:32:58 +0100

    v1.2
    Adding post ID's and classes.

--- loop.php
+++ loop.php
@@ -1,4 +1,4 @@
-<article>
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<h2><a href="<?php esc_url( the_permalink() ); ?>" title="Permalink to <?php the_title(); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
 	<time datetime="<?php the_time( 'Y-m-d' ); ?>" pubdate><?php the_date(); ?></time> <?php comments_popup_link('Leave a Comment', '1 Comment', '% Comments'); ?>
 	<?php the_content(); ?>
--- single.php
+++ single.php
@@ -7,7 +7,7 @@
 
 <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 
-<article>
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
 	<h2><?php the_title(); ?></h2>
 	<time datetime="<?php the_time( 'Y-m-d' ); ?>" pubdate><?php the_date(); ?> <?php the_time(); ?></time> <?php comments_popup_link('Leave a Comment', '1 Comment', '% Comments'); ?>
